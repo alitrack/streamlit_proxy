f72ff248e8414d1c114b12767fab81915daf7003
Steven Li <steven@Stevens-MBP.lan>
2020-04-16 11:00:42 +0800

    update

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,12 +1,22 @@
 # Traitlet configuration file for jupyter-notebook.
 
 c.ServerProxy.servers = {
-    'streamlit_hello': {
-        'command': ['streamlit', 'run','hello.py','--browser.serverAddress=0.0.0.0', '--browser.gatherUsageStats=False', '--server.enableCORS=False'],
+    'demo-self-driving': {
+        'command': ['streamlit', 'run', 'https://raw.githubusercontent.com/streamlit/demo-self-driving/master/app.py',"--browser.serverAddress=0.0.0.0", "--browser.gatherUsageStats=False", "--server.enableCORS=False"],
+        'port':8507
         'timeout': 120,
         'launcher_entry': {
             'enabled': True,
-            'title': 'streamlit_hello',
+            'title': 'demo self driving',
         },
     },
+    'hello': {
+        'command': ['streamlit', 'hello', "--browser.serverAddress=0.0.0.0", "--browser.gatherUsageStats=False", "--server.enableCORS=False"],
+        'port':8508
+        'timeout': 120,
+        'launcher_entry': {
+            'enabled': True,
+            'title': 'streamlit hello',
+        },
+    },    
 }
