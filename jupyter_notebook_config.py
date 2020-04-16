9fa74a55d33006be1a6384ee8a54c67986694d76
Steven Lee <admin@alitrack.com>
2020-04-16 09:49:25 +0800

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -3,6 +3,7 @@
 c.ServerProxy.servers = {
     'streamlit_hello': {
         'command': ['streamlit', 'hello'],
+        'timeout': 1000,
         'launcher_entry': {
             'enabled': True,
             'title': 'streamlit_hello',
